4cc479f0902f6b878a8d4ca23810b3e27049d984
Haydn Stokes <haydnstokes92@gmail.com>
2021-12-15 19:05:14 +0000

    Added top up feature

--- lib/oystercard-2.rb
+++ lib/oystercard-2.rb
@@ -1,5 +1,11 @@
 class Oystercard
-  def balance
-    0
+  attr_reader :balance
+
+  def initialize
+   @balance = 0
+  end
+
+  def top_up(amount)
+   @balance = @balance + amount
   end
 end
--- spec/oystercard-2_spec.rb
+++ spec/oystercard-2_spec.rb
@@ -2,6 +2,11 @@ require 'oystercard-2'
 
 describe Oystercard do
   it 'has a balance of zero' do
-    expect(subject.balance).to eq 0
+    expect(subject.balance).to eq(0)
+  end
+  describe '#top_up' do
+    it 'increments the balance' do
+      expect { subject.top_up(10) }.to change { subject.balance }.by (10)
+    end
   end
 end
